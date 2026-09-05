a8dfee51cb029c0608864c2fa370ce33b791ab12
vaboger234-code <vaboger234@gmail.com>
2026-09-06 01:01:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname)));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.send("Topup Center is Live!");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
